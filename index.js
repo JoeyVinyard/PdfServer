4921ffce2f944a45c00f63335403cf34ee5b2dd9
JoeyVinyard <vinyardjoseph@gmail.com>
2020-01-26 18:40:57 -0500

    Switch to pupetter

--- index.js
+++ index.js
@@ -2,11 +2,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const htmlpdf = require('html-pdf');
-const fs = require('fs');
-const path = require('path');
-const os = require('os');
 const se = require("./scheduleExport");
+const convertHTMLToPDF = require("pdf-puppeteer");
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -15,17 +12,20 @@ app.use(cors());
 app.post('/pdf', function (req, res) {
     const schedule = req.body.data;
     const htmlData = se.exportSchedule(schedule);
-    const tmpDirPath = fs.mkdtempSync(os.tmpdir()+path.sep);
-    const tempPdfPath = path.join(tmpDirPath,"output.pdf"); 
     
-    htmlpdf.create(htmlData, {
-        "format": "Letter",
-        "orientation": "landscape",
-        "border": "0.25in",
-        "zoomFactor": 0.5
-    }).toFile(tempPdfPath, (err, resp) => {
-        let f = fs.readFileSync(tempPdfPath);
-        res.send(f);
+    convertHTMLToPDF(htmlData, (pdf) => {
+        res.send(pdf);
+    }, {
+        width: "10.5in",
+        height: "8in",
+        margin: {
+            top: "0.2in",
+            right: "0.2in",
+            bottom: "0.2in",
+            left: "0.2in"
+        }
+    }).catch((err) => {
+        console.error(err);
     });
 });
 
