93c97e37461ab7b487cc535eff16f1e6cfa9e616
JoeyVinyard <vinyardjoseph@gmail.com>
2020-01-29 01:46:01 -0500

    Fixed weird display bug in pdfs

--- index.js
+++ index.js
@@ -18,25 +18,34 @@ app.post('/pdf', function (req, res) {
     
     const tmpDirPath = fs.mkdtempSync(os.tmpdir()+path.sep);
     const tempPdfPath = path.join(tmpDirPath,"output.pdf"); 
-
+    
     puppeteer.launch().then((browser) => {
-        return browser.newPage();
-    }).then((page) => {
-        page.setContent(htmlData).then(() => {
-            page.pdf({
-                format: "Letter",
-                landscape: true,
-                margin: {
-                    top: "0.4in",
-                    right: "0.4in",
-                    bottom: "0.4in",
-                    left: "0.4in"
-                }
-            }).then((b) => {
-                res.send(b);
-            })
+        browser.newPage().then((page) => {
+            page.setContent(htmlData).then(() => {
+                page.pdf({
+                    format: "Letter",
+                    landscape: true,
+                    margin: {
+                        top: "0.4in",
+                        right: "0.4in",
+                        bottom: "0.4in",
+                        left: "0.4in"
+                    },
+                    printBackground: true
+                }).then((b) => {
+                    browser.close().then(() => {
+                        res.send(b);
+                    }).catch((err) => {
+                        throw err;
+                    })
+                }).catch((err) => {
+                    throw err;
+                });
+            });
         });
-    });
+    }).catch((err) => {
+        console.error(err);
+    })
 });
 
 app.get('/', function (req, res) {
--- scheduleExport.js
+++ scheduleExport.js
@@ -8,10 +8,10 @@ const FONT = "{{FONT}}"
 exports.exportSchedule = (data) => {
     let generatedHTML = [];
 
-    let baseTable = "<html><body>{{TABLE}}</body><style>body{margin: 0} table{border:1px solid black;border-spacing:0;border-collapse:collapse;width:100%} .label{font-size: 18px} th{font-size:{{FONT}}px}th{border:1px solid black}td{border-bottom:1px solid black}.name{border:1px solid black; font-size: 18px; padding-right: 5px}.left{border-right:1px solid black}.hbuffer{border:1px solid black;min-width:2px}.shaded{background-color:grey}.vbuffer .name,.vbuffer .hbuffer{height: 5px; border:1px solid black}</style></html>"
+    let baseTable = "<html><body>{{TABLE}}</body><style>body{margin: 0} table{table-layout: auto; border-spacing: 0; display: block; border: 1px solid black;} table * {font-size: 13px;border-inline-width: 0;} thead * {border-bottom: 1px solid black;} td {display: table-cell; background-color: transparent} tr {display: table-row} td .name{font-size: 14px} .name p {margin-right: 10px} .hbuffer {border-right: 1px solid black;} .time{text-align: center; background-color: white} .label {background-color: white} .label p {font-weight: bold; font-size: 16px; border: none} .label {border-right: 1px solid black;} .left {border-right: 1px solid black;} .hbuffer{min-width:70px}.shaded{background-color: #999} .vbuffer td {height: 3px;} .offRow {background-color: #eee} .onRow {background-color: white}</style></html>"
 
     data.sheets.forEach((ps, i) => {
-        generatedHTML.push(exportSheet(ps,data.sheetIds[i],data.timeIncrement));
+      generatedHTML.push(exportSheet(ps,data.sheetIds[i],data.timeIncrement));
     });
 
     baseTable = baseTable.replace(TABLE, generatedHTML.join("<div style='page-break-before: always;'></div>"));
@@ -23,8 +23,8 @@ const exportSheet = (sheet, id, timeIncrement) => {
 
     let baseTable = "<table>{{TABLE}}</table>"
 
-    let headerRow = "<tr><th class='label'>{{SHEET_LABEL}}</th><th class='hbuffer'></th>{{TIME_COLUMNS}}</tr>";
-    let baseTimeColumn = "<th colspan=2>{{TIME}}</th>";
+    let headerRow = "<thead><tr><td colspan=2 class='label'><p>{{SHEET_LABEL}}</p></td>{{TIME_COLUMNS}}</tr><thead>";
+    let baseTimeColumn = "<td class='time' colspan=2>{{TIME}}</td>";
     let timeHeaders = "";
 
     let tc = generateTimeColumns(sheet, timeIncrement);
@@ -42,20 +42,24 @@ const exportSheet = (sheet, id, timeIncrement) => {
         return r;
       });
     
-    let shiftRows = sheet.shifts.map((shift) => {
-        let s = "<tr><td class='name'>{{NAME}}</td><td class='hbuffer'>{{TIME}}</td>".replace(NAME, shift.empId).replace(TIME, `${timeToString(shift.startTime,false,false)} -${timeToString(shift.endTime,false,false)}`);
-        tc.forEach((t) => {
-            s += `<td class='left ${shouldShade(t,shift,true) ? "shaded" : ""}'></td><td class='${shouldShade(t,shift,false) ? "shaded" : ""}'></td>`
+    let shiftRows = sheet.shifts.map((shift,ind) => {
+        let s = `<tr class='row ${ind%2==1 ? "offRow" : "onRow"}'><td class='name'><p>{{NAME}}</p></td><td class='hbuffer'>{{TIME}}</td>`.replace(NAME, shift.empId).replace(TIME, `${timeToString(shift.startTime,false,false)}-${timeToString(shift.endTime,false,false)}`);
+        tc.forEach((t,i) => {
+            s += `<td class='${i!=0 ? "left" : ""} ${shouldShade(t,shift,true) ? "shaded" : ind%2==1 ? "offRow" : "onRow"}'></td><td class='${shouldShade(t,shift,false) ? "shaded" : ind%2==1 ? "offRow" : "onRow"}'></td>`
         })
         s += "</tr>";
         return s;
     });
 
     
-    let shifts = shiftRows.join("<tr class='vbuffer'><td class='name'></td><td class='hbuffer'></td>" + tc.map(() => "<td class='left'></td><td class=''></td>").join("") + "</tr>");
+    let shifts = [];
+    shiftRows.forEach((r,i) => {
+      shifts.push(`<tr class='vbuffer ${i%2==1 ? "offRow" : "onRow"}'><td class='name'></td><td class='hbuffer'></td>` + tc.map((v,ind) => `<td class='${ind==0 ? "" : "left"} ${i%2==1 ? "offRow" : "onRow"}'></td><td class='${i%2==1 ? "offRow" : "onRow"}'></td>`).join("") + "</tr>");
+      shifts.push(r);
+      shifts.push(`<tr class='vbuffer ${i%2==1 ? "offRow" : "onRow"}'><td class='name'></td><td class='hbuffer'></td>` + tc.map((v,ind) => `<td class='${ind==0 ? "" : "left"} ${i%2==1 ? "offRow" : "onRow"}'></td><td class='${i%2==1 ? "offRow" : "onRow"}'></td>`).join("") + "</tr>");
+    });
     
-    baseTable = baseTable.replace(FONT, tc.length > 32 ? "14" : "16");
-    baseTable = baseTable.replace(TABLE, headerRow+shifts);
+    baseTable = baseTable.replace(TABLE, headerRow+`<tbody>${shifts.join("")}</tbody>`);
     return baseTable;
 }
 
@@ -100,9 +104,5 @@ const generateTimeColumns = (sheet, timeIncrement) => {
   }
 
 const timeToString = (time, space = true, epoch=true) => {
-    return `${time.hours == 12 || time.hours == 0 ? "12" : formatHour(time.hours%12)}:${time.minutes < 10 ? "0" + time.minutes : time.minutes}${space ? " " : ""}${epoch ? time.hours>=12 ? "PM" : "AM" : ""}`;
-}
-
-const formatHour = (h) => {
-    return h < 10 ? " "+h : h;
+    return `${time.hours == 12 || time.hours == 0 ? "12" : time.hours%12}:${time.minutes < 10 ? "0" + time.minutes : time.minutes}${space ? " " : ""}${epoch ? time.hours>=12 ? "PM" : "AM" : ""}`;
 }
